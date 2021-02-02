ba3108e49ae8e0287d95a63cab5f483f1c9c6297
HL <justhl@qq.com>
2021-02-02 14:37:37 +0800

    optimize

--- devtools/src/main/java/real/droid/devtools/DevToolsActivity.java
+++ devtools/src/main/java/real/droid/devtools/DevToolsActivity.java
@@ -3,16 +3,28 @@ package real.droid.devtools;
 import android.app.Activity;
 import android.os.Bundle;
 
-import real.droid.devtools.ui.dir.DirListActivity;
-import real.droid.libx.core.IntentX;
+import real.droid.devtools.ui.main.MainFragment;
+import real.droid.libx.core.NavigatorX;
 
 public class DevToolsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dt_activity_dev_tools);
-        findViewById(R.id.view_dir).setOnClickListener(v -> {
-            IntentX.on(this).className(getPackageName(), DirListActivity.class).startActivity();
-        });
+        setContentView(R.layout.dt_activity_container);
+        NavigatorX.on(this).container(R.id.container).push(new MainFragment());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        NavigatorX.on(this).destroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavigatorX navigatorX = NavigatorX.on(this);
+        if (!navigatorX.pop()) {
+            super.onBackPressed();
+        }
     }
 }
---
+++ devtools/src/main/java/real/droid/devtools/ui/main/MainFragment.java
@@ -0,0 +1,35 @@
+package real.droid.devtools.ui.main;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import real.droid.devtools.R;
+import real.droid.devtools.ui.dir.DirListFragment;
+import real.droid.libx.core.FragmentX;
+import real.droid.libx.core.NavigatorX;
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dt_fragment_main, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getView().findViewById(R.id.tv_file_look).setOnClickListener(v -> {
+            //data dir
+            String internalDataPath = getActivity().getFilesDir().getParentFile().getAbsolutePath();
+            String externalDataPath = getActivity().getExternalFilesDir(null).getParentFile().getAbsolutePath();
+            NavigatorX navigatorX = NavigatorX.on(getActivity());
+
+            navigatorX.push(FragmentX.on(new DirListFragment()).
+                    addArg(DirListFragment.KEY_TITLES, "内部目录;外部目录").
+                    addArg(DirListFragment.KEY_DIRS, internalDataPath + ";" + externalDataPath)
+                    .fragment());
+        });
+    }
+}
